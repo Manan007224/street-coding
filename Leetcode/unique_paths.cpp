dfb8d2a14b530f0ada4a4e7b19c78e5bab137ce2
Manan Maniyar <mmaniyar@sfu.ca>
2019-03-10 21:32:09 -0700

    added unique_paths.cpp

---
+++ Leetcode/unique_paths.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+
+class Solution {
+public:
+  int uniquePaths(int m, int n) {
+  	vii dp(m,vi(n));
+  	for(int i=0; i<m; i++) dp[i][0] = 1;
+  	for(int j=0; j<n; j++) dp[0][j] = 1;
+  	for(int i=1; i<m; i++) {
+  		for(int j=1; j<n; j++) {
+  			dp[i][j] = dp[i-1][j] + dp[i][j-1];
+  		}
+  	}
+  	return dp[m-1][n-1];
+  }
+};
+
+int main() {
+	Solution sln;
+	cout << sln.uniquePaths(1,2) << endl;
+	return 0;
+}

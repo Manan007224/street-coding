5802b5fb640ebce7d18e0eb27116271c84d46d17
Manan Maniyar <mmaniyar@sfu.ca>
2019-04-07 15:07:04 -0700

    updated wildcard_matching.cpp

--- Leetcode/wildcard_matching.cpp
+++ Leetcode/wildcard_matching.cpp
@@ -1,27 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef vector<int> vi;
-typedef vector<vi> vii;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
 
 class Solution {
 public:
   bool isMatch(string s, string p) {
   	if(s.length()==0) return p.length()==0 || p=="*";
   	int a = s.length(), b = p.length();
-  	vii dp(a+1,vi(b+1,-1));
+    vvb dp(a+1,vb(b+1,0));
   	dp[a][b]=1;
-  	return go(s,p,0,0,dp);     
-  }
-  int go(string &s,string &p,int i,int j,vii &dp){
-  	if(dp[i][j]!=-1) return dp[i][j];
-  	bool res=0;
-  	if(p[j]=='*'){
-  		res |= go(s,p,i,j+1,dp);
-  		res |= (i<s.length() && go(s,p,i+1,j,dp));
-  	}
-  	else if(p[j]=='?') res |= (i<s.length() && go(s,p,i+1,j+1,dp));
-  	else if(p[j]==s[i]) res |= (i<s.length() && go(s,p,i+1,j+1,dp));
-  	return dp[i][j]=res;
+    for(int i=s.length(); i>=0; i--) {
+      for(int j=p.length()-1; j>=0; j--) {
+        if(p[j]=='*') {
+          if(i<s.length()) dp[i][j] = dp[i+1][j]; 
+          dp[i][j] = dp[i][j] || dp[i][j+1];
+        }
+        else if(p[j]=='?' || p[j]==s[i]) {
+          dp[i][j] = (i<s.length() && dp[i+1][j+1]);
+        }
+      }
+    }
+    return dp[0][0];    
   }
 };
 
@@ -36,11 +36,14 @@ int main() {
 	string p3 = "a*c?b";
 	string s4 = "d";
 	string p4 = "";
+  string ts = "abc";
+  string tp = "a*";
 	Solution sln;
 	cout<<sln.isMatch(s,p)<<endl;
 	cout<<sln.isMatch(s1,p1)<<endl;
 	cout<<sln.isMatch(s2,p2)<<endl;
 	cout<<sln.isMatch(s3,p3)<<endl;
 	cout<<sln.isMatch(s4,p4)<<endl;
+  cout << sln.isMatch(ts,tp) << endl;
 	return 0;
 }

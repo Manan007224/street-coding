41d1e7bc6c33da500ca357e81649be3cd911aad5
Manan Maniyar <maniyarmanan1996@gmail.com>
2019-04-29 00:01:44 -0700

    minor changes

--- Algorithms/bit_range.cpp
+++ Algorithms/bit_range.cpp
@@ -1,5 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+typedef pair<int,int> pii;
+typedef long long i64;
+#define pb push_back
+#define fi first
+#define se second
+
 struct BIT_Range {
   int N;
   vector<int> val1, val2;

ec8dbba5b84674898d55d17a1b02d0aeb5abb41d
Manan Maniyar <maniyarmanan1996@gmail.com>
2019-04-28 23:53:20 -0700

    some changes

--- Leetcode/cheapest_flight_k_stops.cpp
+++ Leetcode/cheapest_flight_k_stops.cpp
@@ -9,39 +9,39 @@ typedef long long i64;
 #define se second
 typedef unordered_map<int, vector<pii>> Graph;
 
-// class Solution {
-// public:
-//   int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
-//     Graph G;
-//    	for(auto f: flights) {
-//    		G[f[0]].pb({f[1], f[2]});
-//    	}
-//    	int cost = INT_MAX;
-//    	vector<bool> visited(n, 0);
-//    	dfs(src, dst, K, visited, G, cost, 0);
-//    	return (cost == INT_MAX ? -1 : cost);
-//   }
-
-//   void dfs(int src, int dst, int k, vector<bool> &visited, Graph &G, int &cost, int used) {
-//   	if(src == dst) {
-//   		cost = used;
-//   		return;
-//   	}
-//   	if(k==0)
-//   		return;
-//   	visited[src] = 1;
-//   	for(auto n: G[src]) {
-//   		if(!visited[n.fi]) {
-//   			if(used + n.se <= cost) {
-//   				dfs(n.fi, dst, k-1, visited, G, cost, used + n.se);
-//   			}
-//   		}
-//   	}
-//   	visited[src] = 0;
-//   }
-// };
-
 class Solution {
+public:
+  int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
+    Graph G;
+   	for(auto f: flights) {
+   		G[f[0]].pb({f[1], f[2]});
+   	}
+   	int cost = INT_MAX;
+   	vector<bool> visited(n, 0);
+   	dfs(src, dst, K, visited, G, cost, 0);
+   	return (cost == INT_MAX ? -1 : cost);
+  }
+
+  void dfs(int src, int dst, int k, vector<bool> &visited, Graph &G, int &cost, int used) {
+  	if(src == dst) {
+  		cost = used;
+  		return;
+  	}
+  	if(k==0)
+  		return;
+  	visited[src] = 1;
+  	for(auto n: G[src]) {
+  		if(!visited[n.fi]) {
+  			if(used + n.se <= cost) {
+  				dfs(n.fi, dst, k-1, visited, G, cost, used + n.se);
+  			}
+  		}
+  	}
+  	visited[src] = 0;
+  }
+};
+
+class Solution1 {
 public:
 
 	struct node {

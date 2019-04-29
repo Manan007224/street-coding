83cd7e4f63ef5a56a857c885b5e4178472ade82c
Manan Maniyar <maniyarmanan1996@gmail.com>
2019-04-29 16:37:59 -0700

    re

--- Leetcode/cheapest_flight_k_stops.cpp
+++ Leetcode/cheapest_flight_k_stops.cpp
@@ -9,37 +9,37 @@ typedef long long i64;
 #define se second
 typedef unordered_map<int, vector<pii>> Graph;
 
-// class Solution {
-// public:
-//   int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
-//     Graph G;
-//    	for(auto f: flights) {
-//    		G[f[0]].pb({f[1], f[2]});
-//    	}
-//    	int cost = INT_MAX;
-//    	vector<bool> visited(n, 0);
-//    	dfs(src, dst, K, visited, G, cost, 0);
-//    	return (cost == INT_MAX ? -1 : cost);
-//   }
+class Solution {
+public:
+  int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
+    Graph G;
+   	for(auto f: flights) {
+   		G[f[0]].pb({f[1], f[2]});
+   	}
+   	int cost = INT_MAX;
+   	vector<bool> visited(n, 0);
+   	dfs(src, dst, K, visited, G, cost, 0);
+   	return (cost == INT_MAX ? -1 : cost);
+  }
 
-//   void dfs(int src, int dst, int k, vector<bool> &visited, Graph &G, int &cost, int used) {
-//   	if(src == dst) {
-//   		cost = used;
-//   		return;
-//   	}
-//   	if(k==0)
-//   		return;
-//   	visited[src] = 1;
-//   	for(auto n: G[src]) {
-//   		if(!visited[n.fi]) {
-//   			if(used + n.se <= cost) {
-//   				dfs(n.fi, dst, k-1, visited, G, cost, used + n.se);
-//   			}
-//   		}
-//   	}
-//   	visited[src] = 0;
-//   }
-// };
+  void dfs(int src, int dst, int k, vector<bool> &visited, Graph &G, int &cost, int used) {
+  	if(src == dst) {
+  		cost = used;
+  		return;
+  	}
+  	if(k==0)
+  		return;
+  	visited[src] = 1;
+  	for(auto n: G[src]) {
+  		if(!visited[n.fi]) {
+  			if(used + n.se <= cost) {
+  				dfs(n.fi, dst, k-1, visited, G, cost, used + n.se);
+  			}
+  		}
+  	}
+  	visited[src] = 0;
+  }
+};
 
 class Solution{
 public:
@@ -77,9 +77,26 @@ public:
 	}
 };
 
+class Solution {
+public:
+	int findCheapestPrice(int n, vector<vector<int>> &flights, int src, int dst, int K) {
+		vector<int> dp(n+1, INT_MAX);
+		dp[src] = 0;
+		for(int i=0; i<=K; i++) {
+			vector<int> curr(n+1, INT_MAX);
+			for(auto f: flights) {
+				int u = f[0], v = f[1], w = f[2];
+				curr[v] = min(curr[v], dp[u] + w);
+			}
+			dp = curr;
+		}
+		return dp[dst] == INT_MAX ? -1 : dp[dst];
+	}
+};
+
 int main() {
 	Solution sln;
 	vii t = {{0,1,100}, {1,2,100}, {0,2,500}};
-	cout << sln.findCheapestPrice(3, t, 0, 2, 1) << endl;
+	cout << sln.findCheapestPrice(3, t, 0, 2, 0) << endl;
 	return 0;
 }

b1db583c3803075fb96167ef6aea3d78a85b1f5f
Manan Maniyar <maniyarmanan1996@gmail.com>
2019-04-30 13:29:25 -0700

    updated some stuff

--- Leetcode/flatten_list.cpp
+++ Leetcode/flatten_list.cpp
@@ -1,76 +1,46 @@
-include <bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct Node{
-	string val;
-	Node* l;
-	Node* r;
+struct Node {
+	int val;
+	Node *right, *down;
+	Node(int x): val(x), right(NULL), down(NULL) {} 
 };
 
-Node* newNode(string data){
-	Node* tp = new Node;
-	tp->val = data;
-	tp->l=NULL;
-	tp->r=NULL;
-}
+Node* flatten(Node *curr) {
+	Node *right = curr->right, *down = curr->down;
+	if(down) {
+		curr->right = down;
+		curr->down = NULL;
+		curr = flatten(curr->right);
 
-
-Node* flatten(Node* curr){
-	Node* left = curr->l;
-	Node* right = curr->r;
-	if(right!=NULL){
-		curr->l=right;
-		curr->r=NULL;
-		curr=flatten(right);
 	}
-	if(left!=NULL){
-		curr->l=left;
-		curr->r=NULL;
-		curr=flatten(left);
+	if(right) {
+		curr->right = right;
+		curr->down = NULL;
+		curr = flatten(curr->right);
 	}
 	return curr;
 }
 
-void runner(Node** root){
-	Node* curr = *root;
-	flatten(*root);
-}
-
-// void flatten(Node** head){
-// 	stack<Node*> st;
-// 	Node* tp = *head;
-// 	st.push(tp);
-// 	while(!st.empty()){
-// 		Node* curr = st.top();
-// 		st.pop();
-// 		if(curr->l!=NULL)
-// 			st.push(curr->l);
-// 		if(curr->r!=NULL)
-// 			st.push(curr->r);
-// 		tp->l=curr;
-// 		tp->r=NULL;
-// 		tp=tp->l;
-// 	}
-// }
-
-int main(){
-	Node* root=newNode("a");
-	root->l=newNode("b");
-	root->l->l = newNode("c");
-	root->l->l->r=newNode("k");
-	root->l->l->l=newNode("d");
-	root->l->l->l->l=newNode("e");
-	root->l->l->l->l->r=newNode("l");
-	root->l->r=newNode("f");
-	root->l->r->l=newNode("g");
-	root->l->r->l->l=newNode("j");
-	root->l->r->r=newNode("h");
-	root->l->r->r->l=newNode("i");
-	Node* ans = root;
+int main() {
+	Node *root = new Node(1);
+	root->down = new Node(2);
+	root->down->down = new Node(3);
+	root->down->down->right = new Node(4);
+	root->down->down->right->right = new Node(5);
+	root->right = new Node(6);
+	root->right->right = new Node(7);
+	root->right->right->down = new Node(8);
+	root->right->right->down->right = new Node(9);
+	root->right->right->right = new Node(10);
+	root->right->right->right->right = new Node(11);
+	root->right->right->right->right->down = new Node(12);
+	Node *ans = root;
 	flatten(root);
-	while(ans!=NULL){
-		cout<<ans->val<<endl;
-		ans=ans->l;
+	while(ans!=NULL) {
+		cout << ans->val << endl;
+		ans = ans->right;
 	}
 	return 0;
-}
+}	
